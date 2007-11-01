36249112e082da6931275f59006ffb8629d0d713
karl_wessel <karl_wessel@ccb5d51d-3e1d-0410-9886-bf3c9037c363>
2007-11-01 15:51:16 +0000

    ConnectionManager: - moved distance checking from ConnectionManager to BaseConnectionManager. Derived classes of BaseConnectionManager now only need to override "calcInterfDist()" - made BaseConnectionManager 3D compatible - removed TestConnectionManager
    Test cases:
    - removed test/test-cc-connections (replaced by 
    test/connectionManager)
    - updated exp-output of connectionManager test
    
    Multichannel example:
    - moved position parameters of multiChannels 
    omnetpp.ini to the mobility module
    
    Makemakefiles:
    - added multiChannel to makemakefiles
    - removed test-cc-connections from makemakefiles
    
    
    git-svn-id: https://mixim.svn.sourceforge.net/svnroot/mixim/trunk@273 ccb5d51d-3e1d-0410-9886-bf3c9037c363

--- base/connectionManager/ConnectionManager.h
+++ base/connectionManager/ConnectionManager.h
@@ -9,88 +9,17 @@
 class ConnectionManager : public BaseConnectionManager
 {
 protected:
-	typedef std::vector<NicEntries> RowVector;
-	typedef std::vector<RowVector> NicMatrix;
-
-   /** @brief Registry of all Nics
-    *
-    * This matrix keeps all Nics according to their position.  It
-    * allows to restrict the position update to a subset of all nics.
-    */
-    NicMatrix nicMatrix;
-    
-    /**
-     * Distance that helps to find a node under a certain
-     * position. Can be larger then @see maxInterferenceDistance to
-     * allow nodes to be placed into the same square if the playground
-     * is too small for the grid speedup to work. */
-    double findDistance;
-
-    /**
-     * further cached values
-     */
-    unsigned maxX, maxY;
-
-protected:
-	/**
-	 * @brief This method is called by "registerNic()" after the nic has been
-	 * registered. That means that the NicEntry for the nic has already been 
-	 * created and added to nics map.
-	 * 
-	 * @param nicID - the id of the NicEntry
-	 */
-	virtual void registerNicExt(int nicID);
-	
 	
 	/** 
 	 * @brief Calculate interference distance
 	 */
 	virtual double calcInterfDist();
 	
-	/**
-	 * @brief Updates the connections of the nic with "nicID".
-	 * 
-	 * This method is called by "updateNicPos()" after the 
-	 * new Position is stored in the corresponding nic.
-	 * 
-	 * @param nicID the id of the NicEntry
-	 * @param oldPos the old position of the nic
-	 * @param newPos the new position of the nic
-	 */
-	virtual void updateConnections(int nicID, const Coord* oldPos, const Coord* newPos);
-	
-	/**
-     * find the next larger coordinate in grid, return true if the
-     * connections in this position should be updated.
-     */
-	bool increment(unsigned max, unsigned src, unsigned* target);
-	
-	/**
-     * find the next smaller coordinate in grid, return true if the
-     * connections in this position should be updated.
-     */
-    bool decrement(unsigned max, unsigned src, unsigned* target);
-    
-    /** @brief Manages the connections of a registered nic. */ 
-    void updateNicConnections(NicEntries& nmap, NicEntry* nic);
-
-    /**
-     * check connections of a nic in the grid
-     */
-    void checkGrid(unsigned oldX, unsigned oldY,
-                   unsigned newX, unsigned newY,
-                   int id);
 public:
 	/**
 	 * @brief Constructor
 	 **/
 	Module_Class_Members(ConnectionManager, BaseConnectionManager, 0);
-	
-	/**
-	 * @brief Reads init parameters and calculates a maximal interfence
-	 * distance
-	 **/
-	virtual void initialize(int stage);
 };
 
 #endif /*CONNECTIONMANAGER_H_*/
