f2c2f705fc9df1d1d84f1500416cb070131127c7
Jérôme Rousselot <jerome.rousselot@csem.ch>
2009-06-25 14:51:52 +0200

    Cleaned up example directory. Removed lots of old files.

--- examples/ieee802154a/runBER.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 ; 
-do 
-  echo $i ; 
-  ./ieee802154a -r $i ; 
-  mv omnetpp.vec vectors/vector-run_$i.vec ; 
-done
-
--- examples/ieee802154a/runMUI.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in 1 2 3 4 5 6 7 8 9 10  ; 
-do 
-  echo $i ; 
-  ./ieee802154a -r $i ; 
-  mv omnetpp.vec vectors/vector-run_$i.vec ; 
-done
-
