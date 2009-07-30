3a7c2f8d90a1f5057c7b54a892bfc8d46e3177bb
Jérôme Rousselot <jerome.rousselot@csem.ch>
2009-07-30 10:19:13 +0200

    Script to split akaroa results into csv parameter files.

---
+++ examples/ieee802154a/cleanak.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# This script reformats akaroa simulation results.
+# It generates one csv file per parameter.
+# Data files should have the name ak-$config-$run.out
+
+CONFIG="maicirclen"
+#CONFIG="berdistance"
+
+NBRUNS=$((`ls ak-$CONFIG-*.out | wc -l`-1))
+NBPARAMS=`grep   ^[[:space:]] ak-$CONFIG-0.out | wc -l`
+
+# create destination files
+for p in `seq 1 $NBPARAMS`; do
+#  touch $CONFIG-$p.csv
+  echo "Param    Estimate       Delta  Conf TotalObs TransObs" > $CONFIG-$p.csv
+done
+
+# extract data from each run file
+for r in `seq 0 $NBRUNS`; do
+  for p in `seq 1 $NBPARAMS`; do
+    grep ^[[:space:]]\\+$p ak-$CONFIG-$r.out >> $CONFIG-$p.csv
+  done
+done
+
+
