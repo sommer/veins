f1da1b64d149589e14c34489434f0d5d069e75e8
Jérôme Rousselot <jerome.rousselot@csem.ch>
2009-11-20 10:56:43 +0100

    Plot ebn0 graph with new ebn0 analytical method.

--- examples/ieee802154a/ebn0.R
+++ examples/ieee802154a/ebn0.R
@@ -9,6 +9,7 @@ psrRSValues <- c(0.97061, 0.97061, 0.97061, 0.97061, 0.97061,
 		0.97061, 0.97061, 0.969377, 0.963361, 0.907644,
 		0.698511, 0.359492, 0.103714, 0.0218919, 0,
 		0, 0)
+
 psrNoRSValues <- c(0.964188, 0.965016, 0.969944, 0.941074, 0.866947,
 		0.837347,0.698542, 0.511625, 0.282612,  0.0915956,
 		0.0334867, 0.00547897, 0.000303711, 0, 0,
@@ -33,26 +34,33 @@ EbValues <- pathlosses*Epulse
 EbN0Values <- 10*log10( EbValues / noiseValues )
 EbN0Values <- c(EbN0Values, -5)
 
+# binSize = 2
+perRSValues <- c( 1, 1,  1,  1,  1, 0.325, 0.01, 0.01)
+EbN0Values  <- c(-6, 0, 10, 12, 14,    16,   18, 20)
+# binSize = 1
+perRSValues <- c( 1, 1,  1, 1,   1,  1,  1, 0.64, 0.01, 0.01, 0.01, 0.01)
+EbN0Values  <- c(-6, 0, 10, 11, 12, 13, 14,   15,  16,    18,   19, 21)
 par(mar= c(5, 6, 4, 2) + 0.1)
 
-plot(c(0, 30),  c(1, 1E-3), type="n", las=1, xlab="Eb/N0 (dB)", ylab="", 
+plot(c(0, 20),  c(1, 1E-3), type="n", las=1, xlab="Eb/N0 (dB)", ylab="", 
 	cex.lab=2, cex.axis=2, cex=2, log="y", lwd=lineWidth) # setup figure
-lines(EbN0Values, 1-psrNoRSValues, type="b", col="darkblue", pch=21, cex=symbolSize, lwd=lineWidth)
-lines(EbN0Values, 1-psrRSValues, type="b", col="darkgreen", pch=22, cex=symbolSize, lwd=lineWidth)
+#lines(EbN0Values, 1-psrNoRSValues, type="b", col="darkblue", pch=21, cex=symbolSize, lwd=lineWidth)
+#lines(EbN0Values, 1-psrRSValues, type="b", col="darkgreen", pch=22, cex=symbolSize, lwd=lineWidth)
+lines(EbN0Values, perRSValues, type="b", col="darkgreen", pch=22, cex=symbolSize, lwd=lineWidth)
+
 lines(epflEbN0, epflPER_EDoptNoDrift, type="l", col="black",  cex=symbolSize, lwd=lineWidth)
 grid(col="grey")
 lines(epflEbN0, epflPER_EDfixDrift,  type="b", pch=23, col="darkorange", cex=symbolSize, lwd=lineWidth)
 lines(c(17, 17), c(1, 1E-4), lty=5, type="l", col="darkred", cex=symbolSize, lwd=lineWidth)
 mtext("Packet Error Rate", side=3, line=1, adj=0, cex=2, lwd=lineWidth)
-legend(x="bottomright",legend=c("No error correction", 
-				"With error correction",
+legend(x="bottomright",legend=c("With error correction",
 				"CEA-LETI SNR Limit",
 				"EPFL MATLAB EDfix, drift",
 				"EPFL MATLAB EDOpt, no drift"
 				),
-	lty=c(0, 0, 5, 0, 1), # a line only for the last one
-	col=c("darkblue", "darkgreen", "darkred", "darkorange", "black"),
-	pch=c(21,22,NA_integer_,23,NA_integer_), # no symbol for third and last ones
+	lty=c(0, 5, 0, 1), # a line only for the last one
+	col=c("darkgreen", "darkred", "darkorange", "black"),
+	pch=c(22,NA_integer_,23,NA_integer_), # no symbol for third and last ones
 	cex=2, pt.cex=symbolSize, lwd=lineWidth)
 dev.off()
 
