a66220cffda19ca2d2cbaa84480fd7815562cfe6
Jérôme Rousselot <jerome.rousselot@csem.ch>
2010-02-08 13:58:12 +0100

    Added aggregation transport layer.

---
+++ modules/messages/AggrPkt.cc
@@ -0,0 +1,41 @@
+#include "AggrPkt.h"
+
+Register_Class(AggrPkt);
+
+void AggrPkt::setStoredPacketsArraySize(unsigned int size) { }
+
+unsigned int AggrPkt::getStoredPacketsArraySize() const {
+	return storedPackets.size();
+}
+
+// should not be used -- implemented only because omnet++ expects it
+pApplPkt& AggrPkt::getStoredPackets(unsigned int k) {
+	pApplPkt pkt = storedPackets.front();
+	return pkt ;
+//	return storedPackets.front()& ;
+}
+//	{ return storedPackets.at(k); }
+
+
+// should not be used -- implemented only because omnet++ expects it
+void AggrPkt::setStoredPackets(unsigned int k, const pApplPkt& storedPackets_var) { }
+//	cMessage* msg = reinterpret_cast<cMessage*>(storedPackets_var);
+//	take(msg); // update ownership
+//	storedPackets[k] = storedPackets_var;
+//}
+
+void AggrPkt::storePacket(pApplPkt& msg) {
+	take(msg); // update ownership
+	storedPackets.push_back(msg);
+}
+
+pApplPkt& AggrPkt::popFrontPacket() {
+	pApplPkt pkt = storedPackets.front();
+	storedPackets.pop_front();
+	drop(pkt); // update ownership
+	return pkt;
+}
+
+bool AggrPkt::isEmpty() {
+	return storedPackets.size() == 0;
+}
