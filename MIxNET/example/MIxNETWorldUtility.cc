3f6e68f098a8629ea5a47df68332a7eaa3397ef0
Michael Swigulski <mustang289@sourceforge.net>
2010-09-09 18:37:00 +0200

    - made getMiXiMMACAddr const - implemented MIxNETWorldUtility, first working version

--- MIxNET/example/MIxNETWorldUtility.cc
+++ MIxNET/example/MIxNETWorldUtility.cc
@@ -21,11 +21,33 @@ MIxNETWorldUtility::~MIxNETWorldUtility() {
 void MIxNETWorldUtility::addMACAddrPair(const MACAddress& inetAddr,
 										int miximAddr)
 {
+	// convert INET-MAC-address to a string-representation
+	std::string inetAddrString = inetAddr.str();
 
+	// add an entry from INET-MAC-address to MiXiM-MAC-address
+	inetToMiximMACAddr[inetAddrString] = miximAddr;
+
+	// add an entry from MiXiM-MAC-address to INET-MAC-address
+	miximToInetMACAddr[miximAddr] = inetAddrString;
 }
 
-int MIxNETWorldUtility::getMiximMACAddr(const MACAddress& inetAddr)
+int MIxNETWorldUtility::getMiximMACAddr(const MACAddress& inetAddr) const
 {
+	// convert INET-MAC-address to a string-representation
+	std::string inetAddrString = inetAddr.str();
 
-	return 0;
+	// search map for an entry that matches the passed key
+	std::map<std::string, int>::const_iterator it =
+			inetToMiximMACAddr.find(inetAddrString);
+
+	// if not found, return error value;
+	// else return the mapped value
+	if (it == inetToMiximMACAddr.end())
+	{
+		return 0;
+	}
+	else
+	{
+		return it->second;
+	}
 }
--- MIxNET/example/MIxNETWorldUtility.h
+++ MIxNET/example/MIxNETWorldUtility.h
@@ -10,15 +10,40 @@
 
 #include <BaseWorldUtility.h>
 #include <MACAddress.h>
+#include <string>
+#include <map>
 
 class MIxNETWorldUtility: public BaseWorldUtility {
+protected:
+
+	/**
+	 * @brief Mapping from INET-MAC-addresses to MiXiM-MAC-addresses
+	 */
+	std::map<std::string, int> inetToMiximMACAddr;
+
+	/**
+	 * @brief Mapping from MiXiM-MAC-addresses to INET-MAC-addresses
+	 */
+	std::map<int, std::string> miximToInetMACAddr;
+
+
 public:
 	MIxNETWorldUtility();
 	virtual ~MIxNETWorldUtility();
 
+	/**
+	 * @brief Stores a pair of corresponding MAC-addresses
+	 */
 	virtual void addMACAddrPair(const MACAddress& inetAddr, int miximAddr);
 
-	virtual int getMiximMACAddr(const MACAddress& inetAddr);
+	/**
+	 * @brief Returns the MiXiM-MAC-address that corresponds to the passed
+	 * INET-MAC-address
+	 *
+	 * @return Returns the corresponding MiXiM-MAC-address if an entry to
+	 * the passed INET-MAC-address exists, else returns an error value.
+	 */
+	virtual int getMiximMACAddr(const MACAddress& inetAddr) const;
 };
 
 #endif /* MIXNETWORLDUTILITY_H_ */
