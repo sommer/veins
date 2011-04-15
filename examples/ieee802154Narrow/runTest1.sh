56e01f89b2c1b656e06c3fcc1d9417ede5410a76
Karl Wessel <karl_wessel@sourceforge.net>
2011-04-15 10:23:49 +0200

    - changed "runTest*.sh" scripts of ieee802154Narrow example to support parallel execution - added plots zoomed to one packet for channelPlots of ieee802154a example

--- examples/ieee802154Narrow/runTest1.sh
+++ examples/ieee802154Narrow/runTest1.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-
 rm -f results/Test1*
-./ieee802154Narrow -c Test1-A -u Cmdenv 
-./ieee802154Narrow -c Test1-B -u Cmdenv 
-./ieee802154Narrow -c Test1-C -u Cmdenv 
-./ieee802154Narrow -c Test1-D -u Cmdenv 
-./ieee802154Narrow -c Test1-E -u Cmdenv 
+opp_runall $1 ./ieee802154Narrow -c Test1-A -u Cmdenv -r 0..19
+opp_runall $1 ./ieee802154Narrow -c Test1-B -u Cmdenv -r 0..19
+opp_runall $1 ./ieee802154Narrow -c Test1-C -u Cmdenv -r 0..19
+opp_runall $1 ./ieee802154Narrow -c Test1-D -u Cmdenv -r 0..19
+opp_runall $1 ./ieee802154Narrow -c Test1-E -u Cmdenv -r 0..19
