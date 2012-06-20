3b5c84c4acba267b5f340e78d32f4d0a4b901e9b
Christoph Sommer <christoph.sommer@uibk.ac.at>
2012-06-20 11:34:35 -0700

    Update documentation

--- src/modules/world/annotations/AnnotationDummy.h
+++ src/modules/world/annotations/AnnotationDummy.h
@@ -1,11 +1,12 @@
 //
-// AnnotationDummy - workaround to visualize annotations
-// Copyright (C) 2010 Christoph Sommer <christoph.sommer@informatik.uni-erlangen.de>
+// Copyright (C) 2006-2012 Christoph Sommer <christoph.sommer@uibk.ac.at>
 //
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
+// Documentation for these modules is at http://veins.car2x.org/
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,7 +15,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
 
 #ifndef WORLD_ANNOTATION_ANNOTATIONDUMMY_H
@@ -23,9 +24,15 @@
 #include <omnetpp.h>
 
 /**
- * AnnotationDummy is just a workaround to visualize annotations
+ * @brief
+ * Dummy module instantiated by AnnotationManager for drawing.
+ *
+ * See the Veins website <a href="http://veins.car2x.org/"> for a tutorial, documentation, and publications </a>.
  *
  * @author Christoph Sommer
+ *
+ * @see AnnotationManager
+ *
  */
 class AnnotationDummy : public cSimpleModule
 {
