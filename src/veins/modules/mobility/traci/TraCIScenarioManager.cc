c343c3fca82f0857591e2911a3bc9660269b2b71
Florian Hagenauer <hagenauer@ccs-labs.org>
2016-04-13 16:03:44 -0700

    Don't forget parking cars

--- src/veins/modules/mobility/traci/TraCIScenarioManager.cc
+++ src/veins/modules/mobility/traci/TraCIScenarioManager.cc
@@ -756,7 +756,7 @@ void TraCIScenarioManager::processVehicleSubscription(std::string objectId, TraC
 			ASSERT(varType == TYPE_STRINGLIST);
 			uint32_t count; buf >> count;
 			MYDEBUG << "TraCI reports " << count << " active vehicles." << endl;
-			ASSERT(count == drivingVehicleCount);
+			ASSERT(count == activeVehicleCount);
 			std::set<std::string> drivingVehicles;
 			for (uint32_t i = 0; i < count; ++i) {
 				std::string idstring; buf >> idstring;
