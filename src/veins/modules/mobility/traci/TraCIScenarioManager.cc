df924a2a18ac4d947f6684b6751825aa13b28aba
Christoph Sommer <sommer@ccs-labs.org>
2017-02-22 14:53:55 +0100

    traci: allow SUMO 0.29.0

--- src/veins/modules/mobility/traci/TraCIScenarioManager.cc
+++ src/veins/modules/mobility/traci/TraCIScenarioManager.cc
@@ -257,11 +257,11 @@ void TraCIScenarioManager::init_traci() {
 		uint32_t apiVersion = version.first;
 		std::string serverVersion = version.second;
 
-		if ((apiVersion == 10) || (apiVersion == 11) || (apiVersion == 13)) {
+		if ((apiVersion == 10) || (apiVersion == 11) || (apiVersion == 13) || (apiVersion == 14)) {
 			MYDEBUG << "TraCI server \"" << serverVersion << "\" reports API version " << apiVersion << endl;
 		}
 		else {
-			error("TraCI server \"%s\" reports API version %d, which is unsupported. We recommend using SUMO 0.28.0", serverVersion.c_str(), apiVersion);
+			error("TraCI server \"%s\" reports API version %d, which is unsupported. We recommend using SUMO 0.29.0", serverVersion.c_str(), apiVersion);
 		}
 
 	}
