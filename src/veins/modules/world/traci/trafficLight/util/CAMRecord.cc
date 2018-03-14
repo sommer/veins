957541ba536e89db3046b8c6bc51120f2ed91f9d
Tobias <hardes@ccs-labs.org>
2018-03-14 14:11:23 +0100

    adds comment to traffic light classes

--- src/veins/modules/world/traci/trafficLight/util/CAMRecord.cc
+++ src/veins/modules/world/traci/trafficLight/util/CAMRecord.cc
@@ -1,9 +1,19 @@
-/*
- * CAMRecord.cc
- *
- *  Created on: Nov 24, 2015
- *      Author: dominik.buse
- */
+//
+// Copyright (C) 2015 Dominik Buse <dbuse@mail.uni-paderborn.de>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
 
 #include "veins/modules/world/traci/trafficLight/util/CAMRecord.h"
 
