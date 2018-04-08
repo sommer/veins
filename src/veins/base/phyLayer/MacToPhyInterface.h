1263f543ecfcb2c016ad29464c55fd1e560b357d
Gurjashan Pannu <pannu@ccs-labs.org>
2018-04-08 22:36:44 +0200

    add unicast support

--- src/veins/base/phyLayer/MacToPhyInterface.h
+++ src/veins/base/phyLayer/MacToPhyInterface.h
@@ -36,6 +36,15 @@ public:
 		/** @brief Stores the id on which classes extending BasePhy should
 		 * continue their own kinds.*/
 		LAST_BASE_PHY_KIND,
+		/** @brief PHY-RXSTART.indication. Used in ack procedure for unicast
+		 */
+		PHY_RX_START,
+		/** @brief PHY-RXEND.indication and Rx was successful
+		 */
+		PHY_RX_END_WITH_SUCCESS,
+		/** @brief PHY-RXEND.indication and Rx failed
+		 */
+		PHY_RX_END_WITH_FAILURE,
 	};
 
 public:
