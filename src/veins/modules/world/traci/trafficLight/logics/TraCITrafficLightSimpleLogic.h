5af2b078eea0552e433cdc17349cb473311d1f99
hardes <l1743947@nwytg.com>
2018-07-18 17:13:23 +0200

    renamed SimpleLogic and AbstractLogic files according to the module names

--- src/veins/modules/world/traci/trafficLight/logics/TraCITrafficLightSimpleLogic.h
+++ src/veins/modules/world/traci/trafficLight/logics/TraCITrafficLightSimpleLogic.h
@@ -22,8 +22,8 @@
 #define TRACITRAFFICLIGHTSIMPLELOGIC_H_
 
 
+#include <veins/modules/world/traci/trafficLight/logics/TraCITrafficLightAbstractLogic.h>
 #include "veins/base/utils/FindModule.h"
-#include "veins/modules/world/traci/trafficLight/logics/AbstractLogic.h"
 #include "veins/modules/world/traci/trafficLight/TraCITrafficLightInterface.h"
 
 
