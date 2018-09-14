126fdfc753c9e2c6dc50f0bc94a062d15a7465c1
Dominik S. Buse <buse@ccs-labs.org>
2018-09-14 19:07:35 +0200

    add traci vehicle signal header and tests

---
+++ subprojects/veins_catch/src/traci/VehicleSignal.cc
@@ -0,0 +1,65 @@
+#include "catch/catch.hpp"
+#include "veins/modules/mobility/traci/VehicleSignal.h"
+
+using Veins::VehicleSignal;
+using Veins::VehicleSignalSet;
+
+SCENARIO("vehicle signal set gets correctly build correctly from integer bitfield", "[vehiclesignals]")
+{
+    GIVEN("A valid integer bitfield of vehicle signals")
+    {
+        uint32_t bitfield = 0;
+        INFO("Blinker left is bit " << static_cast<uint32_t>(VehicleSignal::blinker_left));
+        bitfield += 1 << static_cast<int>(VehicleSignal::blinker_left);
+        INFO("Break light is bit " << static_cast<uint32_t>(VehicleSignal::brakelight));
+        bitfield += 1 << static_cast<int>(VehicleSignal::brakelight);
+        INFO("native bitfield: " << std::bitset<32>(bitfield).to_string());
+        CAPTURE(sizeof(size_t));
+
+        THEN("A vehicle signal set constructed from it has the right flags set")
+        {
+            VehicleSignalSet signals{bitfield};
+            CAPTURE(signals.to_string());
+            REQUIRE(signals.test(VehicleSignal::blinker_left));
+            REQUIRE(signals.test(VehicleSignal::brakelight));
+
+            AND_THEN("the vehicle signal set can be converted back to the originial traci int")
+            {
+                REQUIRE(signals.to_ulong() == bitfield);
+            }
+        }
+    }
+}
+
+SCENARIO("vehicle signal set produces correct integer bitfield", "[vehiclesignals]")
+{
+    GIVEN("A vehicle signal set with enabled blinker right and backdrive")
+    {
+        VehicleSignalSet signals;
+        signals.set(VehicleSignal::blinker_right);
+        signals.set(VehicleSignal::backdrive);
+
+        THEN("The resulting bitset should be 000000010000001")
+        {
+            REQUIRE(signals.to_string() == "000000010000001");
+        }
+    }
+    GIVEN("A vehicle signal set to be undefined")
+    {
+        VehicleSignalSet signals;
+        signals.set(VehicleSignal::undefined);
+
+        THEN("The resulting bitset should be 100000000000000")
+        {
+            REQUIRE(signals.to_string() == "100000000000000");
+        }
+    }
+    GIVEN("A vehicle signal set with no signal set")
+    {
+        VehicleSignalSet signals;
+        THEN("The signal set should be equal to zero")
+        {
+            REQUIRE(signals.to_ulong() == 0);
+        }
+    }
+}
