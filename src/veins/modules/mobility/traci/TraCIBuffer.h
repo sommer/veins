a0e467ceb22b2b322048967ad267447c880d5586
Dominik S. Buse <buse@ccs-labs.org>
2018-10-31 15:26:21 +0100

    double and int time representation for TraCIBuffer

--- src/veins/modules/mobility/traci/TraCIBuffer.h
+++ src/veins/modules/mobility/traci/TraCIBuffer.h
@@ -114,9 +114,12 @@ public:
     std::string str() const;
     std::string hexStr() const;
 
+    static void setTimeAsDouble(bool val) { timeAsDouble = val; }
+
 private:
     std::string buf;
     size_t buf_index;
+    static bool timeAsDouble;
 };
 
 template <>
