8e5705953d12d8b9792488aaafd0faa65dd7b43f
Max Schettler <schettle@mail.upb.de>
2018-11-20 18:14:11 +0100

    Remove unused omnet simulation signal name definitions

--- src/veins/base/connectionManager/ChannelAccess.cc
+++ src/veins/base/connectionManager/ChannelAccess.cc
@@ -36,7 +36,7 @@
 using std::endl;
 using namespace Veins;
 
-const simsignal_t ChannelAccess::mobilityStateChangedSignal = registerSignal(MIXIM_SIGNAL_MOBILITY_CHANGE_NAME);
+const simsignal_t ChannelAccess::mobilityStateChangedSignal = registerSignal("veinsmobilityStateChanged");
 
 BaseConnectionManager* ChannelAccess::getConnectionManager(cModule* nic)
 {
