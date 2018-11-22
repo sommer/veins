c2f662166190bb7683efba97f19bf08aad348265
Agon Memedi <memedi@ccs-labs.org>
2018-11-22 11:19:52 +0100

    add parameter "recurse" to getSubmodulesOfType

--- src/veins/base/utils/FindModule.h
+++ src/veins/base/utils/FindModule.h
@@ -108,12 +108,17 @@ public:
  * @brief Return a vector containing pointers to all submodules of parentModule of type T
  */
 template <class T>
-std::vector<T*> getSubmodulesOfType(cModule* parentModule)
+std::vector<T*> getSubmodulesOfType(cModule* parentModule, bool recurse = false)
 {
     std::vector<T*> result;
     for (cModule::SubmoduleIterator iter(parentModule); !iter.end(); iter++) {
         auto mm = dynamic_cast<T*>(*iter);
         if (mm != nullptr) result.push_back(mm);
+        if (recurse) {
+            for (auto m : getSubmodulesOfType<T>(*iter, recurse)) {
+                result.emplace_back(m);
+            }
+        }
     }
     return result;
 }
