129f93e3a623dd5bbfadcb88f729c16705287df7
Max Schettler <schettle@mail.upb.de>
2018-11-23 16:48:16 +0100

    Add make_unique helper

---
+++ src/veins/base/utils/util.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <memory>
+#include <utility>
+
+#ifdef __cpp_lib_make_unique
+using make_unique = std::make_unique;
+#else
+/**
+ * User-defined implementation of std::make_unique.
+ *
+ * Until Veins builds on C++14 this provides equivalent functionality.
+ */
+template<typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args)
+{
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+#endif
