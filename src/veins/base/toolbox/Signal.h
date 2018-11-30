16cc3a4280bbe78a0467155d4b3b3355cfb2dfb6
Max Schettler <schettler@ccs-labs.org>
2018-11-30 13:40:43 +0100

    Reorder signal members

--- src/veins/base/toolbox/Signal.h
+++ src/veins/base/toolbox/Signal.h
@@ -36,59 +36,50 @@ public:
     Signal(Spectrum spec, simtime_t start, simtime_t duration);
     ~Signal() = default;
 
+    Spectrum getSpectrum() const;
+
     double& at(size_t index);
     const double& at(size_t index) const;
     double& atFrequency(double freq);
     const double& atFrequency(double freq) const;
-
-    Spectrum getSpectrum() const;
-
     double getFreqAt(size_t freqIndex) const;
+    double* getValues();
+    size_t getNumValues() const;
+    double getMax() const;
+    double getMinInRange(size_t freqIndexLow, size_t freqIndexHigh) const;
+    double getMaxInRange(size_t freqIndexLow, size_t freqIndexHigh) const;
+
+    double getData(size_t index) const;
     double getDataFreqAt(size_t freqIndex) const;
-
     size_t getDataStart() const;
-
     size_t getDataEnd() const;
-
+    double* getDataValues();
+    size_t getNumDataValues() const;
+    size_t getDataOffset() const;
+    double getDataMin() const;
+    double getDataMax() const;
+    void setData(size_t index, double value);
     void setDataStart(size_t index);
     void setDataEnd(size_t index);
     void setDataNumValues(size_t num);
 
-    double* getValues();
-    double* getDataValues();
-
-    size_t getNumValues() const;
-    size_t getNumDataValues() const;
-
-    size_t getDataOffset() const;
-
-    void setData(size_t index, double value);
-    double getData(size_t index) const;
-
-    void setCenterFrequencyIndex(size_t index);
     size_t getCenterFrequencyIndex() const;
     double getAtCenterFrequency() const;
-
+    void setCenterFrequencyIndex(size_t index);
     bool greaterAtCenterFrequency(double threshold);
     bool smallerAtCenterFrequency(double threshold);
 
     uint16_t getNumAnalogueModelsApplied() const;
+    AnalogueModelList* getAnalogueModelList() const;
+    size_t getNumAnalogueModels() const;
+    void setAnalogueModelList(AnalogueModelList* list);
     void applyAnalogueModel(uint16_t index);
     void applyAllAnalogueModels();
 
-    void setAnalogueModelList(AnalogueModelList* list);
-    void setSenderPos(Coord pos);
-    void setReceiverPos(Coord pos);
-
-    AnalogueModelList* getAnalogueModelList() const;
-    size_t getNumAnalogueModels() const;
     Coord getSenderPos() const;
     Coord getReceiverPos() const;
-
-    void setSendingStart(simtime_t start);
-    void setDuration(simtime_t duration);
-    void setPropagationDelay(simtime_t_cref delay);
-    void setTiming(simtime_t start, simtime_t duration);
+    void setSenderPos(Coord pos);
+    void setReceiverPos(Coord pos);
 
     simtime_t_cref getSendingStart() const;
     simtime_t getSendingEnd() const;
@@ -96,35 +87,11 @@ public:
     simtime_t getReceptionEnd() const;
     simtime_t_cref getDuration() const;
     simtime_t_cref getPropagationDelay() const;
-
     bool hasTiming() const;
-
-    double getDataMin() const;
-    double getMinInRange(size_t freqIndexLow, size_t freqIndexHigh) const;
-
-    double getMax() const;
-    double getDataMax() const;
-    double getMaxInRange(size_t freqIndexLow, size_t freqIndexHigh) const;
-
-    Signal& operator=(const double value);
-    Signal& operator=(const Signal& other);
-
-    Signal& operator+=(const Signal& other);
-    Signal& operator+=(const double value);
-
-    Signal& operator-=(const Signal& other);
-    Signal& operator-=(const double value);
-
-    Signal& operator*=(const Signal& other);
-    Signal& operator*=(const double value);
-
-    Signal& operator/=(const Signal& other);
-    Signal& operator/=(const double value);
-
-    friend std::ostream& operator<<(std::ostream& os, const Signal& s);
-
-    friend inline simtime_t calculateStart(const Signal& lhs, const Signal& rhs);
-    friend inline simtime_t calculateDuration(const Signal& lhs, const Signal& rhs);
+    void setSendingStart(simtime_t start);
+    void setDuration(simtime_t duration);
+    void setPropagationDelay(simtime_t_cref delay);
+    void setTiming(simtime_t start, simtime_t duration);
 
     uint64_t getBitrate() const;
     void setBitrate(uint64_t rate);
@@ -160,6 +127,26 @@ public:
     /** @brief Saves the arrival sender module information form message. */
     void setReceptionSenderInfo(const cMessage* const pMsg);
 
+    Signal& operator=(const double value);
+    Signal& operator=(const Signal& other);
+
+    Signal& operator+=(const Signal& other);
+    Signal& operator+=(const double value);
+
+    Signal& operator-=(const Signal& other);
+    Signal& operator-=(const double value);
+
+    Signal& operator*=(const Signal& other);
+    Signal& operator*=(const double value);
+
+    Signal& operator/=(const Signal& other);
+    Signal& operator/=(const double value);
+
+    friend std::ostream& operator<<(std::ostream& os, const Signal& s);
+
+    friend inline simtime_t calculateStart(const Signal& lhs, const Signal& rhs);
+    friend inline simtime_t calculateDuration(const Signal& lhs, const Signal& rhs);
+
 private:
     Spectrum spectrum;
 
@@ -180,6 +167,7 @@ private:
 
     AnalogueModelList* analogueModelList = nullptr;
     uint16_t numAnalogueModelsApplied = 0;
+
     Coord senderPos = Coord(0, 0);
     Coord receiverPos = Coord(0, 0);
 
