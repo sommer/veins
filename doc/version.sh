d9208315d4043e0dd2f10a0416458b4eaf182bac
Julian Heinovski <heinovski@ccs-labs.org>
2018-12-20 10:20:33 +0100

    Add automatic version number to documentation

---
+++ doc/version.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+TAG=$(git describe --tags 2> /dev/null)
+if [[ ($? -eq 0) && ($TAG =~ ^veins-) ]]; then
+    echo $TAG | sed -n 's/^veins-\(.*\)$/\1/p'
+else
+    git rev-parse --short HEAD
+fi
