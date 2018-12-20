1c3fc9d60221e78d512d6e548f8761744784fcea
Julian Heinovski <heinovski@ccs-labs.org>
2018-12-20 11:20:31 +0100

    Use readme content for mainpage

--- src/veins/veins.h
+++ src/veins/veins.h
@@ -19,7 +19,7 @@
 //
 
 /**
- * @mainpage %Veins - The open source vehicular network simulation framework.
+ * %Veins - The open source vehicular network simulation framework.
  *
  * See the %Veins website <a href="http://veins.car2x.org/"> for a tutorial, documentation, and publications </a>.
  */
