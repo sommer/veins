d692e05c803ee904c9e9731f1f4a68bf8b292858
Julian Heinovski <heinovski@ccs-labs.org>
2019-02-13 11:15:20 +0100

    Add documentation to version script

--- doc/version.sh
+++ doc/version.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Generates the version number used for the doxygen documentation
+#
+# Uses git describe to analyze the current HEAD of the repository.
+# Due to --tags, it tries to refer to the latest tag in the history and
+# describes the difference in commits to this tag.
+# Due to --aways, the command produces an output even if no tag exits.
+#
+# Example: veins-5a1-16-gd81b4a14a0
+# The current HEAD points at the commit d81b4a14a0, which is 16 commits
+# newer than the tag veins-5a1 it is based on.
+#
+# In case git describe fails, the version is empty.
+
 TAG=$(git describe --tags --always 2> /dev/null)
 if [[ ($? -eq 0) && ($TAG =~ ^veins-) ]]; then
     echo $TAG | sed -n 's/^veins-\(.*\)$/\1/p'
