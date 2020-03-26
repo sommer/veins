e51a8643d0f5456450829d1d76d86d08e4066b4d
Julian Heinovski <heinovski@ccs-labs.org>
2020-03-26 09:20:16 +0100

    Add callgrind as profiling tool to run script

--- subprojects/veins_inet/src/scripts/run.in.py
+++ subprojects/veins_inet/src/scripts/run.in.py
@@ -40,7 +40,7 @@ def relpath(s):
 
 parser = argparse.ArgumentParser('Run a Veins simulation')
 parser.add_argument('-d', '--debug', action='store_true', help='Run using opp_run_dbg (instead of opp_run)')
-parser.add_argument('-t', '--tool', metavar='TOOL', dest='tool', choices=['lldb', 'gdb', 'memcheck'], help='Wrap opp_run execution in TOOL (lldb, gdb or memcheck)')
+parser.add_argument('-t', '--tool', metavar='TOOL', dest='tool', choices=['lldb', 'gdb', 'memcheck', 'callgrind'], help='Wrap opp_run execution in TOOL (lldb, gdb, memcheck, or callgrind)')
 parser.add_argument('-v', '--verbose', action='store_true', help='Print command line before executing')
 parser.add_argument('--', dest='arguments', help='Arguments to pass to opp_run')
 args, omnet_args = parser.parse_known_args()
@@ -66,6 +66,8 @@ if args.tool == 'gdb':
     prefix = ['gdb', '--args']
 if args.tool == 'memcheck':
     prefix = ['valgrind', '--tool=memcheck', '--leak-check=full', '--dsymutil=yes', '--log-file=valgrind.out']
+if args.tool == 'callgrind':
+    prefix = ['valgrind', '--tool=callgrind', '--log-file=callgrind.out']
 
 cmdline = prefix + [opp_run] + lib_flags + ned_flags + img_flags + omnet_args
 
