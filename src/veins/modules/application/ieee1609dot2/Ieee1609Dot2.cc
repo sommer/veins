7a45b7e54af949a43820ff64b8a230faf0efca13
Datalux <giuseppe.criscione@gmail.com>
2020-04-11 10:45:26 +0200

    Added serial support to Ieee1609Dot2Message

--- src/veins/modules/application/ieee1609dot2/Ieee1609Dot2.cc
+++ src/veins/modules/application/ieee1609dot2/Ieee1609Dot2.cc
@@ -1,24 +1,3 @@
-//
-// Copyright (C) 2006-2011 Christoph Sommer <christoph.sommer@uibk.ac.at>
-//
-// Documentation for these modules is at http://veins.car2x.org/
-//
-// SPDX-License-Identifier: GPL-2.0-or-later
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
 
 #include "veins/modules/application/ieee1609dot2/Ieee1609Dot2.h"
 #include "veins/modules/application/ieee1609dot2/Ieee1609Dot2Message_m.h"
@@ -62,6 +41,7 @@ void Ieee1609Dot2::processSPDU(Ieee1609Dot2Message* spdu)
                     sentMessage = true;
                     // repeat the received traffic update once in 2 seconds plus some random delay
                     spdu->setSenderAddress(myId);
+                    spdu->setSerial(3);
                     scheduleAt(simTime() + 2 + uniform(0.01, 0.2), spdu->dup());
                 }
             break;
@@ -119,6 +99,15 @@ void Ieee1609Dot2::handleSelfMsg(cMessage* msg)
         // send this message on the service channel until the counter is 3 or higher.
         // this code only runs when channel switching is enabled
         sendDown(wsm->dup());
+        wsm->setSerial(wsm->getSerial() + 1);
+        if (wsm->getSerial() >= 3) {
+            // stop service advertisements
+            stopService();
+            delete (wsm);
+        }
+        else {
+            scheduleAt(simTime() + 1, wsm);
+        }
     }
     else {
         DemoBaseApplLayer::handleSelfMsg(msg);
