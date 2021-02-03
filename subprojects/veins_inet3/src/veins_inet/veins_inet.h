516a0ae5e25e12118e24ba5c85f88a039864ec03
Christoph Sommer <sommer@cms-labs.org>
2021-02-03 15:29:39 +0100

    veins_inet3: emit compiler warnings when using unsupported INET versions

--- subprojects/veins_inet3/src/veins_inet/veins_inet.h
+++ subprojects/veins_inet3/src/veins_inet/veins_inet.h
@@ -23,6 +23,7 @@
 #pragma once
 
 #include "veins/veins.h"
+#include "inet/common/INETDefs.h"
 
 // Version number of last release ("major.minor.patch") or an alpha version, if nonzero
 #define VEINS_INET_VERSION_MAJOR 3
@@ -33,6 +34,14 @@
 // Explicitly check Veins version number
 #if !(VEINS_VERSION_MAJOR == 5 && VEINS_VERSION_MINOR >= 1)
 #error Veins version 5.1 or compatible required
+#endif
+
+// Explicitly check INET version number
+#if !(INET_VERSION >= 0x0306 && INET_VERSION < 0x0400)
+#error INET version 3.6 or compatible required
+#endif
+#if !((INET_VERSION == 0x0306) || (INET_VERSION == 0x0307))
+#pragma message ("WARNING: Unsupported INET version. Only INET version 3.6 and 3.7 are supported.")
 #endif
 
 // VEINS_INET_API macro. Allows us to use the same .h files for both building a .dll and linking against it
